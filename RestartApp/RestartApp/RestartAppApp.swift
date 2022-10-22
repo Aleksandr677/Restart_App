c2441bdbda2944f4ce7b161b9b1f145b0a2aa853
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-10-22 16:42:43 +0300

    feat: add the whole project

---
+++ RestartApp/RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Христиченко Александр on 2022-10-20.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestartApp/RestartApp/Screens/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  RestartApp
+//
+//  Created by Христиченко Александр on 2022-10-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
